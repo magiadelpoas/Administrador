158cc9026062284658a66c322cc5818d5901ebcd
Roberto <carlosroart13@gmail.com>
2025-09-20 13:36:57 -0600

    Remove all project files and configurations
    - Deleted all source files, including components, pages, and styles, to reset the project.
    - Removed configuration files such as .gitignore, package.json, and eslint.config.js.
    - Cleared all assets including images, fonts, and JavaScript plugins.
    - This commit effectively resets the project to a clean state.

--- src/Sistema/pages/HomePage/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { HomeView } from '../../views/home/HomeView'
-
-export const HomePage = () => {
-  return (
-    <><HomeView /></>
-  )
-}
--- src/Sistema/views/home/HomeView.jsx
+++
@@ -1,9 +0,0 @@
-
-
-import React from 'react'
-
-export const HomeView = () => {
-  return (
-    <div>Home View</div>
-  )
-}
